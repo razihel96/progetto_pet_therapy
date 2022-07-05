5ede054a31df95e27cb2509c8a24c451c78eb36b
hccsan <72209607+hccsan@users.noreply.github.com>
2022-07-05 20:51:02 +0200

    Added style for registerUser.html etc

--- Progetto_personale/src/main/java/com/example/demo/controller/validator/UserValidator.java
+++ Progetto_personale/src/main/java/com/example/demo/controller/validator/UserValidator.java
@@ -12,34 +12,33 @@ import com.example.demo.model.User;
 @Component
 public class UserValidator implements Validator {
 
-    final Integer MAX_NAME_LENGTH = 100;
-    final Integer MIN_NAME_LENGTH = 2;
+	final Integer MAX_NAME_LENGTH = 100;
+	final Integer MIN_NAME_LENGTH = 2;
 
-    @Override
-    public void validate(Object o, Errors errors) {
-        User user = (User) o;
-        String nome = user.getNome().trim();
-        String cognome = user.getCognome().trim();
-        String indirizzo = user.getIndirizzo().trim();
+	@Override
+	public void validate(Object o, Errors errors) {
+		User user = (User) o;
+		String nome = user.getNome().trim();
+		String cognome = user.getCognome().trim();
+		String email = user.getEmail().trim();
 
+		if (nome.isEmpty())
+			errors.rejectValue("nome", "required");
+		else if (nome.length() < MIN_NAME_LENGTH || nome.length() > MAX_NAME_LENGTH)
+			errors.rejectValue("nome", "size");
 
-        if (nome.isEmpty())
-            errors.rejectValue("nome", "required");
-        else if (nome.length() < MIN_NAME_LENGTH || nome.length() > MAX_NAME_LENGTH)
-            errors.rejectValue("nome", "size");
+		if (cognome.isEmpty())
+			errors.rejectValue("cognome", "required");
+		else if (cognome.length() < MIN_NAME_LENGTH || cognome.length() > MAX_NAME_LENGTH)
+			errors.rejectValue("cognome", "size");
 
-        if (cognome.isEmpty())
-            errors.rejectValue("cognome", "required");
-        else if (cognome.length() < MIN_NAME_LENGTH || cognome.length() > MAX_NAME_LENGTH)
-            errors.rejectValue("cognome", "size");
-        
-        if (indirizzo.isEmpty())
-            errors.rejectValue("indirizzo", "required");
-    	}
+		if (email.isEmpty())
+			errors.rejectValue("email", "required");
+	}
 
-    @Override
-    public boolean supports(Class<?> clazz) {
-        return User.class.equals(clazz);
-    }
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return User.class.equals(clazz);
+	}
 
 }
--- Progetto_personale/src/main/java/com/example/demo/model/User.java
+++ Progetto_personale/src/main/java/com/example/demo/model/User.java
@@ -10,53 +10,49 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "users") // cambiamo nome perchè in postgres user e' una parola riservata
 public class User {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String nome;
 	private String cognome;
-	private String indirizzo;
-	
-	
-	
-	//un utente può intraprendere un solo percorso
+	private String email;
+
+	// un utente può intraprendere un solo percorso
 	@ManyToOne
 	private Percorso percorso;
-	
-	
-	
+
 	/* SETTERS & GETTERS */
 	public Long getId() {
 		return id;
 	}
-	
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
-	
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 	public String getCognome() {
 		return cognome;
 	}
-	
+
 	public void setCognome(String cognome) {
 		this.cognome = cognome;
 	}
 
-	public String getIndirizzo() {
-		return indirizzo;
+	public String getEmail() {
+		return email;
 	}
 
-	public void setIndirizzo(String indirizzo) {
-		this.indirizzo = indirizzo;
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public Percorso getPercorso() {
@@ -66,12 +62,5 @@ public class User {
 	public void setPercorso(Percorso percorso) {
 		this.percorso = percorso;
 	}
-	
-	
-
-	
-	
-	
-	
 
 }
