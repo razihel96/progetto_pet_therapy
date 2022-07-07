4c2e6f96fc72c266b7d54c55512abea76a24e60c
hccsan <72209607+hccsan@users.noreply.github.com>
2022-07-07 17:51:00 +0200

    Fixed packages name, role

--- Progetto_personale/src/main/java/com/example/demo/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.model.User;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-
-}
